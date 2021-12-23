b3983cfe7344382325e860441274d8514c8c21f2
MattTriano <matt.triano@gmail.com>
2021-12-23 12:12:43 -0600

    Caching util code.

--- code/usa_spending.py
+++ code/usa_spending.py
@@ -13,3 +13,10 @@ def get_api_endpoints() -> pd.DataFrame:
     endpoint_df = pd.read_html(endpoint_str)
     endpoint_df = endpoint_df[0].copy()
     return endpoint_df
+
+
+def make_api_call(endpoint: str) -> requests.models.Response:
+    base_url = "https://api.usaspending.gov"
+    api_call = f"{base_url}{endpoint}"
+    res = requests.get(api_call)
+    return res
